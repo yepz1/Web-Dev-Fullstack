13fa262449e6481d8242429ed237ea9f3bcbc147
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-08 11:12:07 -0300

    Create arraysMethods.js
    Learning how all this array methods works.

---
+++ _11_Arrays Javascript/arraysMethods.js
@@ -0,0 +1,29 @@
+var colors = ["red", "green", "blue"];
+
+//adds new element to the end of the array
+colors.push("yellow");
+
+//removes the last element of the array
+colors.pop();
+
+//adds new element to the beginning of the array
+colors.unshift("purple");
+
+//removes the first element of the array
+colors.shift();
+
+//returns the index of the element
+colors.indexOf("red");
+
+//copy some elements from the array
+var newColors = colors.slice(1, 3);
+
+//copy all elements from the array
+var otherColors = colors.slice();
+
+
+
+var todos = ["Buy New Turtle"];
+window.setTimeout(function() {
+  // put all the rest of your JS code from the lecture here
+}, 500);
