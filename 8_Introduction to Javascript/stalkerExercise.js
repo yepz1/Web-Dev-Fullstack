7175b1d0514c0ce7e227dccec89ab8065c487415
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-08 10:48:05 -0300

    Create stalkerExercise.js
    Creepy script to see more indepth how vars works.

---
+++ 8_Introduction to Javascript/stalkerExercise.js
@@ -0,0 +1,7 @@
+var firstName, lastName, age;
+
+firstName = prompt("PLeAsE TEll mE YouR FiRSt nAMe");
+lastName = prompt("NOw tElL mE YOur LaSt NaME");
+age = prompt("Now your age please")
+
+alert("HAHAH I KNOW WHO YOU ARE " + firstName + " " + lastName + " AND I KNOW YOU ARE " + age + " I'M GOING TO FIND YOU")
