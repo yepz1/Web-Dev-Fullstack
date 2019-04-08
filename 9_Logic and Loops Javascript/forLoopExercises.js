66e40fc6ac5a77d77e25d08b7c793e830dcf4485
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-08 10:58:49 -0300

    Create forLoopExercises.js
    Learning how the for loop works.

---
+++ 9_Logic and Loops Javascript/forLoopExercises.js
@@ -0,0 +1,25 @@
+console.log("Printing all numbers between -10 and 19.");
+for(i = -10; i <= 19; i++){
+    console.log(i)
+}
+
+console.log("Printing all even numbers between 10 and 40.");
+for(i = 10; i <= 40; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+console.log("Printing all odd numbers between 300 and 333.");
+for(i = 300; i <= 333; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+console.log("Printing all numbers divisible by 5 and 3 between 5 and 50.");
+for(i = 5; i <= 50; i++){
+    if(i % 5 == 0 && i % 3 == 0){
+        console.log(i);
+    }
+}
