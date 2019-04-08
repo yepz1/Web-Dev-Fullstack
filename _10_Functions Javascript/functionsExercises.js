0a46aec441ff1809d8cfb5a7b6714e7f6ce203b8
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-08 11:09:04 -0300

    Create functionsExercises.js
    Learning how functions works in JS.

---
+++ _10_Functions Javascript/functionsExercises.js
@@ -0,0 +1,21 @@
+function isEven(num) {
+    if(num % 2 == 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function factorial(num) {
+    if (num == 0) {
+        return 1;
+    }
+    else {
+        return (num * factorial(num - 1));
+    }
+}
+
+function kebabToSnake(char) {
+    return char.replace(/-/g, "_");
+}
