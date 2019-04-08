6616c94f640027772c907fa4e51c17562d8d8fff
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-08 18:51:38 -0300

    Create objectMethods.js
    Learning how functions inside objects, which are called methods, works.

---
+++ _12_Objects Javascript/objectMethods.js
@@ -0,0 +1,13 @@
+var stats = {
+    data: [
+        "life",
+        "mana",
+        "exp",
+        "lvl"
+    ],
+    print: function() {
+        this.data.forEach(function(el) {
+            console.log(el);
+        });
+    }
+}
