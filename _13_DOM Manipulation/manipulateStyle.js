c24bdefb572c7299418e4c8673ac86527fcdbfc1
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-09 11:51:23 -0300

    Create manipulateStyle.js
    Uses css class to manipulate the html

---
+++ _13_DOM Manipulation/manipulateStyle.js
@@ -0,0 +1,10 @@
+var p = document.querySelector("p");
+
+// adds css class "big" to "p"
+// p.classList.add("big")
+
+// removes css class "big" from "p"
+// p.classList.remove("big")
+
+// turns on and off css class "big" from "p"
+// p.classList.toggle("big")
