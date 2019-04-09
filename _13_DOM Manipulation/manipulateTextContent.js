23a9bd866877967cc14919f1f8e3e20faa644bf9
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-09 12:25:36 -0300

    Create manipulateTextContent.js
    Learning how to manipulate the text and the content.

---
+++ _13_DOM Manipulation/manipulateTextContent.js
@@ -0,0 +1,16 @@
+var tag = document.getElementsByTagName("p")[0]
+var ul = document.querySelector("ul")
+
+// Overwrites the html text with a new one, returns the text removing any tags between them.
+// ul.textContent
+//
+//  "Warrior"
+//  "Mage"
+//  "Buccaneer"
+
+// Overwrites the html text with a new one, returns the text keeping any tags between them.
+// ul.innerHTML
+//
+//  <li>"Warrior"</li>
+//  <li>"Mage"</li>
+//  <li>"Buccaneer"</li>
