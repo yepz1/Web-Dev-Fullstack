8c7b104ac069032d7acac7493756382aa96809c0
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-09 11:01:53 -0300

    Create importantSelectors.js
    Learning some of the DOM selectors.

---
+++ _13_DOM Manipulation/importantSelectors.js
@@ -0,0 +1,14 @@
+//Select by ID
+var tag = document.getElementById("highlight");
+
+//Select by Class
+var tags = document.getElementsByClassName("bolded");
+
+//Select by tag
+var tagss = document.getElementsByTagName("li");
+
+//Select using css syntax, returns only the first element
+var tagg = document.querySelector(".bolded");
+
+//Select using css syntax, returns all elements
+var taggg = document.querySelectorAll(".bolded");
