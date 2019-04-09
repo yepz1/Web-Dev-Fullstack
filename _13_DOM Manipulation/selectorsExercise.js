6c61b2d1b19d43ae4eda44e1437ebd006eccca50
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-09 11:17:08 -0300

    Create selectorsExercise.js
    Training what I just learned of DOM selectors

---
+++ _13_DOM Manipulation/selectorsExercise.js
@@ -0,0 +1,6 @@
+var first = document.querySelector("p")
+var second = document.querySelector("#first")
+var third = document.querySelector(".special")
+var fourth = document.getElementById("first")
+var fifth = document.getElementsByClassName("special")[0]
+var sixth = document.querySelectorAll("p")[0]
