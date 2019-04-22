795dc5173fd9f97afdbba7ae095e0e04fbd21c4a
Kevin Shibuya <47981817+sadkevin@users.noreply.github.com>
2019-04-22 14:05:21 -0300

    Create eventOn.js

---
+++ _17_Advanced jQuery/eventOn.js
@@ -0,0 +1,9 @@
+$("button").on("mouseenter", function() {
+    $(this).css("font-weight", "bold")
+})
+$("button").on("mouseleave", function() {
+    $(this).css("font-weight", "normal")
+})
+$("button").on("click", function() {
+    alert("button clickd uhu")
+})
